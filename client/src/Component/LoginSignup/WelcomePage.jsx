e114d251fa4dbe0e8a6bc8de3ab6d5eeb6f9f78c
balajis183 <sbalaji2000s@gmail.com>
2024-12-31 12:21:53 +0530

    first commit added client and server for signup and login

---
+++ client/src/Component/LoginSignup/WelcomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WelcomePage = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Welcome to the Website!</h1>
+      <p>You have successfully logged in.</p>
+    </div>
+  );
+};
+
+export default WelcomePage;
